0451b5523921db32519744aa6d3ae951e790feca
Joseph Sammarco <jsammarco@gmail.com>
2014-12-07 18:08:56 -0600

    Added Magnific (FancyBox) to Gallery
    Need to group (rel) galleries still.

--- models/Product.js
+++ models/Product.js
@@ -15,7 +15,10 @@ Product.add({
 	name: { type: String, required: true },
 	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
 	creator: { type: Types.Relationship, ref: 'User', index: true },
-	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
+    createdAt: { type: Types.Datetime, default: Date.now, noedit: true },
+    publishedAt: { type: Types.Datetime, noedit: true , dependsOn: { state: 'published' }},
+	inventory: { type: Types.Number, default: 0 },
+	price: { type: Types.Money, default: 39.99 },
 	image: { type: Types.CloudinaryImage },
 	content: {
 		brief: { type: Types.Html, wysiwyg: true, height: 150 },
@@ -28,5 +31,20 @@ Product.schema.virtual('content.full').get(function() {
 	return this.content.extended || this.content.brief;
 });
 
-Product.defaultColumns = 'name, state|20%, author|20%, publishedDate|20%';
+Product.schema.methods.isPublished = function() {
+    return this.state == 'published';
+}
+
+Product.schema.methods.isInStock = function() {
+    return this.inventory;
+}
+ 
+Product.schema.pre('save', function(next) {
+    if (this.isModified('state') && this.isPublished() && !this.publishedAt) {
+        this.publishedAt = new Date();
+    }
+    next();
+});
+
+Product.defaultColumns = 'name, price|10%, state|10%, creator|20%, publishedDate|20%';
 Product.register();
