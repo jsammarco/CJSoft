5d1cc9d5ce3bb2ba12ba51224e41c788bed06993
Joseph Sammarco <jsammarco@gmail.com>
2014-12-07 18:13:07 -0600

    Added gallery grouping.

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,11 @@
           closeOnContentClick: true,
           image: {
             verticalFit: false
+          },
+          gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
           }
         });
 
